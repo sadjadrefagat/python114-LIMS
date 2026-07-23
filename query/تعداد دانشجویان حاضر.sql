0ed09ad3476ae208ea63fb653ffd44c769517def
sadjadrefagat@gmail.com <sadjadrefagat@gmail.com>
2026-07-23 11:16:13 +0330

    ---

---
+++ query/تعداد دانشجویان حاضر.sql
@@ -0,0 +1,14 @@
+﻿/*
+تعداد دانشجویان حاضر
+*/
+
+SELECT
+	COUNT(DISTINCT SS.StudentRef) Count
+FROM
+	Class C
+	JOIN Session S ON S.ClassRef = C.Id
+	JOIN SessionStudent SS ON SS.SessionRef = S.Id
+WHERE
+	StartDate IS NOT NULL
+	AND
+	EndDate IS NULL
---
+++ query/میانگین زمان برگزاری کلاس در یک ماه اخیر.sql
@@ -0,0 +1,10 @@
+﻿/*
+میانگین زمان برگزاری کلاس در یک ماه اخیر
+*/
+
+SELECT
+	AVG(DATEDIFF(MINUTE, CAST(StartTime AS TIME), CAST(EndTime AS TIME)))
+FROM
+	Session S
+WHERE
+	S.Date >= '1405/04/01'
